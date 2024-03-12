79b7198d102b0330fc10b1aa5951eab257646815
Bethel Fiagbetor <110622280+litoguy@users.noreply.github.com>
2024-03-12 01:29:13 +0000

    First commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.scss"
+
+const Navbar =() =>{
+    return(
+        <div className="navbar">
+        {/* sidebar */}
+        <div className="wrapper">
+            <span>Dev. Bethel </span>
+            <div className="links"></div>
+        </div>
+        </div>
+    )
+}
+
+export default Navbar
